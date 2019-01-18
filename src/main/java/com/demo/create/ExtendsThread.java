5122e1f540861da088b761a581d734dccaa33fa3
McDulliBiBi <523837497@qq.com>
2019-01-18 10:23:04 +0800

    modify demo about creating threads

--- src/main/java/com/demo/create/ExtendsThread.java
+++ src/main/java/com/demo/create/ExtendsThread.java
@@ -8,6 +8,11 @@ public class ExtendsThread extends Thread {
         while(tickets > 0){
             System.out.println("线程" +Thread.currentThread().getName() +
                         "卖出了第" + tickets-- + "张票" );
+            try {
+                Thread.sleep(1000);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
 }
